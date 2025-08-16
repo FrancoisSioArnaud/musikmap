5b05275faed51b0b690e978819c0dcf7015cbf0d
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-17 00:40:16 +0200

    Update views.py

--- frontend/views.py
+++ frontend/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 
 
 def index(request, *args, **kwargs):
-    return render(request, 'templates/frontend/index.html')
+    return render(request, 'frontend/index.html')
 
 # Takes the request and the template (index.html and return the HTML to the client)
+
 
 
