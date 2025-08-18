c486cae40adeb4b102726292584b294b5974f003
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-18 04:51:34 +0200

    Update BoxStartup.js

--- frontend/src/components/MusicBox/OnBoarding/BoxStartup.js
+++ frontend/src/components/MusicBox/OnBoarding/BoxStartup.js
@@ -7,7 +7,7 @@ import Typography from "@mui/material/Typography";
 export default function BoxStartup({ setStage, boxInfo, className }) {
   return (
     <>
-      {boxInfo && Object.keys(boxInfo.box || {}).length > 0 ? (
+      {boxInfo /*&& Object.keys(boxInfo.box || {}).length > 0 */ ? (
         <Paper
           className={className}
           elevation={3}
@@ -42,3 +42,4 @@ export default function BoxStartup({ setStage, boxInfo, className }) {
     </>
   );
 }
+
