9620d2b5077dddccdda5f9ec25973915bbc44dcd
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-20 23:47:39 +0200

    Update models.py

--- box_management/models.py
+++ box_management/models.py
@@ -53,7 +53,6 @@ class Song(models.Model):
     artist = models.CharField(max_length=50)
     spotify_url = models.CharField(max_length=255, null=True, blank=True)
     deezer_url  = models.CharField(max_length=255, null=True, blank=True)
-    platform_id = models.IntegerField(default=1)
     image_url = models.URLField(max_length=200, blank=True)
     duration = models.IntegerField(default=0)  # Duration in seconds
     n_deposits = models.IntegerField(default=0)
@@ -124,6 +123,7 @@ class DiscoveredSong(models.Model):
         Method goal: Returns the id of the user and the id of the deposit used to display it in the admin interface.
         """
         return str(self.user_id) + ' - ' + str(self.deposit_id)
+
 
 
 
