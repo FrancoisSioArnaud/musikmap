b38a791783fabb9cff6d1133e2dd7f1a561192eb
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-21 03:12:16 +0200

    Update views.py

--- box_management/views.py
+++ box_management/views.py
@@ -179,7 +179,7 @@ class GetBox(APIView):
                 request_platform = None
     
             if request_platform:
-                aggreg_url = request.build_absolute_uri("../api_aggregation/aggreg")
+                aggreg_url = request.build_absolute_uri("/api_aggregation/aggreg")
                 headers = {"Content-Type": "application/json"}
                 payload = {
                     "song": {"title": song.title, "artist": song.artist, "duration": song.duration},
@@ -508,6 +508,7 @@ class RevealSong(APIView):
             }
         }
         return Response(data, status=status.HTTP_200_OK)
+
 
 
 
