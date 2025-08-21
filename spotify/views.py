6266cf0b5b39fdb5ef8441ba61cc47cbef2e3b88
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-21 05:49:06 +0200

    Update views.py

--- spotify/views.py
+++ spotify/views.py
@@ -231,8 +231,8 @@ class Search(APIView):
         # Extract the search query from the request data
         search_query = request.data.get('search_query')
 
-        # Search for tracks using the Spotipy client
-        results = sp.search(q=search_query, type='track', limit=50)
+        # Search for tracks using the Spotipy client, limit = nombre de songs requested
+        results = sp.search(q=search_query, type='track', limit=15)
 
         # Extract the track data from the results and create a list of tracks
         tracks = []
@@ -250,4 +250,5 @@ class Search(APIView):
             tracks.append(track)
         # Return the list of tracks as a response
         return Response(tracks, status=status.HTTP_200_OK)
+
 
