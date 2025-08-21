f293188ff4979bec5ddfad6b238afb03c87b26f4
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-21 06:40:14 +0200

    Update SongDisplay.js

--- frontend/src/components/MusicBox/OnBoarding/SongDisplay.js
+++ frontend/src/components/MusicBox/OnBoarding/SongDisplay.js
@@ -164,7 +164,7 @@ export default function SongDisplay({
         const isRevealed = already || Boolean(s?.title && s?.artist);
 
         const card = (
-          <Card key={`card-${idx}`} sx={{ p: 2, background: "linear-gradient(135deg, #FF9900, #FF6900, #FF3D00)" }}>
+          <Card key={`card-${idx}`} sx={{ p: 2 }}>
             {/* 1) deposit_date */}
             <Box
               id="deposit_date"
@@ -198,7 +198,7 @@ export default function SongDisplay({
             {/* 3) deposit_song */}
             {idx === 0 ? (
               // ======= PREMIER DÉPÔT =======
-              <Box id="deposit_song" sx={{ display: "grid", gap: 1, mb: 2 }}>
+              <Box id="deposit_song" sx={{ display: "grid", gap: 1, mb: 2 , background: "linear-gradient(135deg, #FF9900, #FF6900, #FF3D00)"}}>
                 {/* Image carré plein largeur */}
                 <Box sx={{ width: "100%", borderRadius: 1, overflow: "hidden" }}>
                   {s?.img_url && (
