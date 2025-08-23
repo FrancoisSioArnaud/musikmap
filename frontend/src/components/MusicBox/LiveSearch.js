2f370dddf68cced206092d5bb8fb9e640af268ff
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-23 14:21:25 +0200

    Update LiveSearch.js

--- frontend/src/components/MusicBox/LiveSearch.js
+++ frontend/src/components/MusicBox/LiveSearch.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import { getCookie } from "../Security/TokensUtils";
@@ -426,6 +426,7 @@ export default function SongDisplay({
     </Box>
   );
 }
+
 
 
 
