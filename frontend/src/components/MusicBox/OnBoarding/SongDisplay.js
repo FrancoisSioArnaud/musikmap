e7401b2e51b2f48da71d7ec8e49350a747068ec2
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-08-23 18:44:44 +0200

    Update SongDisplay.js

--- frontend/src/components/MusicBox/OnBoarding/SongDisplay.js
+++ frontend/src/components/MusicBox/OnBoarding/SongDisplay.js
@@ -20,12 +20,12 @@ import { UserContext } from "../../UserContext";
 
 export default function SongDisplay({
   dispDeposits,
-  setDispDeposits, // utilisé pour maj après reveal
+  setDispDeposits, 
   isSpotifyAuthenticated,
   isDeezerAuthenticated,
   boxName,
   user,
-  revealCost, // ← nouveau (reçu du parent)
+  revealCost,
 }) {
   const navigate = useRouterNavigate();
   const { setUser } = useContext(UserContext) || {};
