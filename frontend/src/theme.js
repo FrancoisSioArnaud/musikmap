244e331e640ac634590aebe81ae5b73d0f5b94ed
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-09-07 22:01:37 +0200

    Update theme.js

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -155,7 +155,7 @@ const theme = createTheme({
         {
           props: { variant: "depositInteract" }, // <Button variant="gradient">
           style: {
-            background: "linear-gradient(90deg, #FF9900, #FF3D00)",
+            background: "linear-gradient(35deg, #FF9900, #FF3D00)",
             boxShadow: 0px 4px 0px 0px rgba(0, 0, 0, 0.3),
             color: "#000",
             fontWeight: 700,
