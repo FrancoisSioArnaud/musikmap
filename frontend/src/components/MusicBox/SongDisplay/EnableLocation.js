57988f85c24d616bc9d41314dd8f415515b70303
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-09-09 14:32:53 +0200

    Update EnableLocation.js

--- frontend/src/components/MusicBox/SongDisplay/EnableLocation.js
+++ frontend/src/components/MusicBox/SongDisplay/EnableLocation.js
@@ -31,49 +31,52 @@ export default function EnableLocation({
           width: "100vw",
         },
       }}
-      className="modal modal_loc"
+      
     >
-      <Box className="intro">
-        <Box className="icon squaredesign" >
-        </Box>
-        <Typography className="squaredesign" variant="subtitle1" component="span">
-          {boxTitle}
-        </Typography>
-      </Box>
-
-      <Typography variant="h3" component="h1">
-        Localisation
-      </Typography>
-
-      <Typography variant="body1">
-        Pour éviter les tricheurs, les boîtes ne peuvent être ouvertes qu’en étant sur place.
-      </Typography>
-
-      {error ? (
-        <Typography variant="body1" color="error">
-          {error}
-        </Typography>
-      ) : null}
-
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={onAuthorize}
-        disabled={loading}
-        fullWidth
-        startIcon={<KeyIcon />}
-      >
-        {loading ? (
-          <Box sx={{ display: "flex", alignItems: "center", gap: 1, justifyContent: "center" }}>
-            <CircularProgress size={18} />
-            Vérification...
+      <stack className="modal modal_loc">
+        <Box className="intro">
+          <Box className="icon squaredesign" >
           </Box>
-        ) : (
-          "Autoriser"
-        )}
-      </Button>
+          <Typography className="squaredesign" variant="subtitle1" component="span">
+            {boxTitle}
+          </Typography>
+        </Box>
+  
+        <Typography variant="h3" component="h1">
+          Localisation
+        </Typography>
+  
+        <Typography variant="body1">
+          Pour éviter les tricheurs, les boîtes ne peuvent être ouvertes qu’en étant sur place.
+        </Typography>
+  
+        {error ? (
+          <Typography variant="body1" color="error">
+            {error}
+          </Typography>
+        ) : null}
+  
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onAuthorize}
+          disabled={loading}
+          fullWidth
+          startIcon={<KeyIcon />}
+        >
+          {loading ? (
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1, justifyContent: "center" }}>
+              <CircularProgress size={18} />
+              Vérification...
+            </Box>
+          ) : (
+            "Autoriser"
+          )}
+        </Button>
+      </stack>
     </Drawer>
   );
 }
+
 
 
