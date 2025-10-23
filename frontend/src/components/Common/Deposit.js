83eaa1b444e374896e3326317f3e8a36b0ddef95
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-10-23 21:47:34 +0200

    Update Deposit.js

--- frontend/src/components/Common/Deposit.js
+++ frontend/src/components/Common/Deposit.js
@@ -278,41 +278,43 @@ export default function Deposit({
             )}
           </Box>
 
-          {/* textes + Play (ou Skeleton si non révélé) */}
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 1, minWidth: 0 }}>
-            {isRevealed ? (
-              <>
-                <Typography component="h2" variant="h4">
-                  {s.title}
-                </Typography>
-                <Typography component="h3" variant="body">
-                  {s.artist}
-                </Typography>
-                <Button
-                  variant="depositInteract"
-                  className="play playSecondary"
-                  size="large"
-                  onClick={() => openPlayFor(s)}
-                  startIcon={<PlayArrowIcon />}
-                >
-                  Play
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button
-                  variant="depositInteract"
-                  onClick={revealDeposit}
-                  disabled={!user || !user.username}
-                  className="decouvrir"
-                >
-                  <Typography className="points" variant="h4">
-                    {cost}
+          {/* textes + Play*/}
+          <Box className="interact">
+            <Box className="texts">
+              {isRevealed ? (
+                <>
+                  <Typography component="span" className="titre squaredesign" variant="h4">
+                    {s.title}
                   </Typography>
-                  Découvrir
-                </Button>
-              </>
-            )}
+                  <Typography component="span" className="artist squaredesign" variant="body1">
+                    {s.artist}
+                  </Typography>
+                  <Button
+                    variant="depositInteract"
+                    className="play playSecondary"
+                    size="large"
+                    onClick={() => openPlayFor(s)}
+                    startIcon={<PlayArrowIcon />}
+                  >
+                    Play
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    variant="depositInteract"
+                    onClick={revealDeposit}
+                    disabled={!user || !user.username}
+                    className="decouvrir"
+                  >
+                    <Typography className="points" variant="h4">
+                      {cost}
+                    </Typography>
+                    Découvrir
+                  </Button>
+                </>
+              )}
+            </Box>
           </Box>
         </Box>
       </Card>
