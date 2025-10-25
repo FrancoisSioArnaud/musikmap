6b8594e7fdc4321b62301d084730d123c3b5ae1e
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-10-25 20:57:44 +0200

    Update urls.py

--- box_management/urls.py
+++ box_management/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import *
-
+from django.urls import path
+from .views import (
+    BoxMeta, GetBox, Location, CurrentBoxManagement, ManageDiscoveredSongs, RevealSong, UserDepositsView,
+    EmojiCatalogView, PurchaseEmojiView, ReactionView
+)
 
 urlpatterns = [
     path("meta", BoxMeta.as_view(), name="box-meta"),
@@ -10,10 +11,8 @@ urlpatterns = [
     path('current-box-management', CurrentBoxManagement.as_view()),
     path('discovered-songs', ManageDiscoveredSongs.as_view()),
     path('revealSong', RevealSong.as_view(), name="reveal-song"),
-    path('user-deposits', UserDepositsView.as_view(), name="user-deposits"), 
+    path('user-deposits', UserDepositsView.as_view(), name="user-deposits"),
+    path('emojis/catalog', EmojiCatalogView.as_view(), name="emoji-catalog"),
+    path('emojis/purchase', PurchaseEmojiView.as_view(), name="emoji-purchase"),
+    path('reactions', ReactionView.as_view(), name="reactions"),
 ]
-
-
-
-
-
