f304832a88fd63669d69c6e5265525560e3b3149
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-10-26 18:42:21 +0100

    Update LoginPage.js

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -95,7 +95,7 @@ export default function LoginPage() {
           textAlign: "center",
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: "#fa9500" }}>
+        <Avatar sx={{ m: 1, bgcolor: "#7BD528" }}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
@@ -182,6 +182,7 @@ export default function LoginPage() {
     </Container>
   );
 }
+
 
 
 
