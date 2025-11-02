562b2b32a83bd64c99f451abbfeb6c6459fb3245
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-11-02 22:46:15 +0100

    Create FlowboxLayout.js

---
+++ frontend/src/components/Flowbox/FlowboxLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams, Outlet, ScrollRestoration } from "react-router-dom";
+
+export default function FlowboxLayout() {
+  const { boxSlug } = useParams();
+
+  return (
+    <>
+      <Outlet />
+      <ScrollRestoration />
+    </>
+  );
+}
