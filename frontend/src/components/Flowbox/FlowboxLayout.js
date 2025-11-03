0ded1a608efbb900f2bf4b74332d42645bd68dd4
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-11-03 13:29:48 +0100

    Update FlowboxLayout.js

--- frontend/src/components/Flowbox/FlowboxLayout.js
+++ frontend/src/components/Flowbox/FlowboxLayout.js
@@ -3,7 +3,7 @@ import { useParams, Outlet, ScrollRestoration } from "react-router-dom";
 
 export default function FlowboxLayout() {
   const { boxSlug } = useParams();
-
+  consolee.log("[FlowboxLayout] render slug=", boxSlug)
   return (
     <>
       <p>FlowBox layout</p>
