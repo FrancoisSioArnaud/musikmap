350d7aff8ba5859d7411ac0aad2bf98b055e8e70
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2025-11-12 16:09:39 +0100

    Update urls.py

--- box_management/urls.py
+++ box_management/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('get-box/', GetBox.as_view(), name="get-box"),
     path("get-main/<slug:box_url>/", GetMain.as_view(), name="get-main"),
     path('verify-location', Location.as_view()),
-    path('current-box-management', CurrentBoxManagement.as_view()),
     path('discovered-songs', ManageDiscoveredSongs.as_view()),
     path('revealSong', RevealSong.as_view(), name="reveal-song"),
     path('user-deposits', UserDepositsView.as_view(), name="user-deposits"),
@@ -16,6 +15,7 @@ urlpatterns = [
     path('emojis/purchase', PurchaseEmojiView.as_view(), name="emoji-purchase"),
     path('reactions', ReactionView.as_view(), name="reactions"),
 ]
+
 
 
 
