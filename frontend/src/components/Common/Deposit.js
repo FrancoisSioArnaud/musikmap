dab7f7847c623b851fbdbe501aff7bd0fe852449
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-02-09 15:50:04 +0100

    Update Deposit.js

--- frontend/src/components/Common/Deposit.js
+++ frontend/src/components/Common/Deposit.js
@@ -450,35 +450,37 @@ export default function Deposit({
   return (
     <>
       <Card className="deposit deposit_list">
-        <Box className="deposit_infos">
-
-          {showTime && (
-            <Box className="deposit_date">
-              <Typography component="h3" variant="body1">
-                {`Chanson déposée ${naturalDate || ""}${
-                  showUser ? " par :" : ""
-                }`}
-              </Typography>
-            </Box>
-          )}
-
-          {showUser && (
-            <Box
-              className="deposit_user"
-              sx={{ display: "flex", minWidth: 0 }}
-              onClick={() => {
-                if (u?.username) navigate("/profile/" + u.username);
-              }}
-            >
-              <Avatar
-                src={u?.profile_pic_url || undefined}
-                alt={u?.username || "Anonyme"}
-              />
-              <Typography>{u?.username || "Anonyme"}</Typography>
-              {u?.username && <ArrowForwardIosIcon fontSize="small" />}
-            </Box>
-          )}
-        </Box>
+        {(showTime || showUser) && (
+          <Box className="deposit_infos">
+  
+            {showTime && (
+              <Box className="deposit_date">
+                <Typography component="h3" variant="body1">
+                  {`Chanson déposée ${naturalDate || ""}${
+                    showUser ? " par :" : ""
+                  }`}
+                </Typography>
+              </Box>
+            )}
+  
+            {showUser && (
+              <Box
+                className="deposit_user"
+                sx={{ display: "flex", minWidth: 0 }}
+                onClick={() => {
+                  if (u?.username) navigate("/profile/" + u.username);
+                }}
+              >
+                <Avatar
+                  src={u?.profile_pic_url || undefined}
+                  alt={u?.username || "Anonyme"}
+                />
+                <Typography>{u?.username || "Anonyme"}</Typography>
+                {u?.username && <ArrowForwardIosIcon fontSize="small" />}
+              </Box>
+            )}
+          </Box>
+        )}
 
         {/* ----- Section chanson ----- */}
         <Box className="deposit_song">
