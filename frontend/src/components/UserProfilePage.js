8c63ec8e80eaf7076d88692edeaaf00a40e6a664
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-02-12 16:07:18 +0100

    Update UserProfilePage.js

--- frontend/src/components/UserProfilePage.js
+++ frontend/src/components/UserProfilePage.js
@@ -221,7 +221,7 @@ export default function UserProfilePage() {
       ) : (
         <>
           {/* Public: shares only (mounted only after header ready) */}
-          <Typography variant="h6" sx={{ p: "26px 16px 6px 16px" }}>
+          <Typography variant="h5" sx={{ p: "26px 16px 6px 16px" }}>
             {`Partages de ${profileTitleUsername}`}
           </Typography>
           <Shares username={targetUsername} user={user} autoLoad={true} />
@@ -230,5 +230,6 @@ export default function UserProfilePage() {
     </Box>
   );
 }
+
 
 
