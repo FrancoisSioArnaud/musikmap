2a5338cf6435f8fe723d5acea453f85a643882a0
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-03-19 17:23:07 +0100

    Update serializers.py

--- box_management/serializers.py
+++ box_management/serializers.py
@@ -1,11 +1,29 @@
 from rest_framework import serializers
-from .models import Box, Song, Deposit, LocationPoint, DiscoveredSong, Emoji, EmojiRight, Reaction
+from .models import Box, Client, Song, Deposit, LocationPoint, DiscoveredSong, Emoji, EmojiRight, Reaction
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ["id", "name", "background_picture", "created_at", "updated_at"]
 
 
 class BoxSerializer(serializers.ModelSerializer):
+    client_detail = ClientSerializer(source="client", read_only=True)
+
     class Meta:
         model = Box
-        fields = '__all__'
+        fields = [
+            "id",
+            "name",
+            "description",
+            "url",
+            "image_url",
+            "client",
+            "client_detail",
+            "created_at",
+            "updated_at",
+        ]
 
 
 class SongSerializer(serializers.ModelSerializer):
@@ -42,6 +60,7 @@ class EmojiSerializer(serializers.ModelSerializer):
 
 class EmojiRightSerializer(serializers.ModelSerializer):
     emoji = EmojiSerializer()
+
     class Meta:
         model = EmojiRight
         fields = '__all__'
@@ -49,8 +68,7 @@ class EmojiRightSerializer(serializers.ModelSerializer):
 
 class ReactionSerializer(serializers.ModelSerializer):
     emoji = EmojiSerializer()
+
     class Meta:
         model = Reaction
         fields = '__all__'
-
-
