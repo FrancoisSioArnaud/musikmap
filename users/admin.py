9e8dd6ba167cb75209d04f019424d40960a85adc
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-03-27 16:46:23 +0100

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -4,17 +4,87 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import CustomUser
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        # ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'profile_picture')}),
-        ('Personal Info', {'fields': ('email', 'profile_picture', 'preferred_platform', 'points')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "email",
+                    "profile_picture",
+                    "preferred_platform",
+                    "points",
+                    "client",
+                    "client_role",
+                    "portal_status",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
 
-    # Remove the 'first_name' and 'last_name' fields from list_display
-    list_display = ('username', 'email', 'preferred_platform', 'points', 'is_staff')
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "client",
+                    "client_role",
+                    "portal_status",
+                    "is_staff",
+                    "is_superuser",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
 
-
-admin.site.register(CustomUser, CustomUserAdmin)
+    list_display = (
+        "username",
+        "email",
+        "client",
+        "client_role",
+        "portal_status",
+        "preferred_platform",
+        "points",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "client_role",
+        "portal_status",
+        "preferred_platform",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "groups",
+        "client",
+    )
+    search_fields = (
+        "username",
+        "email",
+        "client__name",
+    )
+    autocomplete_fields = ("client",)
+    ordering = ("username",)
