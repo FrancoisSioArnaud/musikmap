c99db736768a7fa1041c52d3b7d1a32886560999
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-03-28 02:22:02 +0100

    Update urls.py

--- la_boite_a_son/urls.py
+++ la_boite_a_son/urls.py
@@ -1,39 +1,34 @@
-#la_boite_a_son/urls.py
-"""la_boite_a_son URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
-
-# Here, dispatch the URLs to the correct app
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('frontend.urls')),
-    path('spotify/', include('spotify.urls')),  # Any address with /spotify/...
-    path('box-management/', include('box_management.urls')),
-    path('deezer/', include('deezer.urls')),
-    path('users/', include('django.contrib.auth.urls')),  # Bundled Django auth URLs
-    path('users/', include('users.urls')),
-    path('api_agg/', include('api_aggregation.urls')),
-    path('oauth/', include('social_django.urls', namespace='social')),  # Social-Auth endpoints
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
 
+    path("index", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="index.html")),
+    path("register", TemplateView.as_view(template_name="index.html")),
+    path("login", TemplateView.as_view(template_name="index.html")),
+    path("profile", TemplateView.as_view(template_name="index.html")),
+    path("profile/<str:username>", TemplateView.as_view(template_name="index.html")),
+    path("library", TemplateView.as_view(template_name="index.html")),
+    path("box/<str:boxName>", TemplateView.as_view(template_name="index.html")),
 
+    re_path(r"^flowbox/.*$", TemplateView.as_view(template_name="index.html")),
+    re_path(r"^client/.*$", TemplateView.as_view(template_name="index.html")),
 
+    path("spotify/", include("spotify.urls")),
+    path("box-management/", include("box_management.urls")),
+    path("deezer/", include("deezer.urls")),
+    path("users/", include("users.urls")),
+    path("users/", include("allauth.urls")),
+    path("api_agg/", include("api_aggregation.urls")),
+    path("oauth/", include("social_django.urls", namespace="social")),
+]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
