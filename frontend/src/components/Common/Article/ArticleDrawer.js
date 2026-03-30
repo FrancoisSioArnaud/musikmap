73d349ac5f4ef5146826d7d81cbc4888117ed302
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-03-30 17:42:25 +0200

    Update ArticleDrawer.js

--- frontend/src/components/Common/Article/ArticleDrawer.js
+++ frontend/src/components/Common/Article/ArticleDrawer.js
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 
-import MarkdownContent from "../Markdown/MarkdownContent";
+import MarkdownContent from "../../Utils/MarkdownContent";
 
 export default function ArticleDrawer({ article, open, onClose, boxSlug }) {
   const previewText = typeof article?.short_text === "string" ? article.short_text : "";
