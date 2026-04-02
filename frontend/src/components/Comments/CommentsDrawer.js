6cb09b582687c57565b4dbaa38cfc745e8de7833
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-04-02 22:48:53 +0200

    Update CommentsDrawer.js

--- frontend/src/components/Comments/CommentsDrawer.js
+++ frontend/src/components/Comments/CommentsDrawer.js
@@ -183,7 +183,9 @@ export default function CommentsDrawer({
 
         <Box className="comments_panel">
           <Box className="intro_small">
-        
+            <Typography variant="h3" component="h3">
+              Commentaires
+            </Typography>
             <Typography variant="subtitle1" component="subtitle1">
               Sois bienveillant et respectueux.
             </Typography>
