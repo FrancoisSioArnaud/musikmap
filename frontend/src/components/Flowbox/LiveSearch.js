15abea2415c4e67da3ef78feee4898b6f14651a1
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-04-04 21:50:42 +0200

    Update LiveSearch.js

--- frontend/src/components/Flowbox/LiveSearch.js
+++ frontend/src/components/Flowbox/LiveSearch.js
@@ -480,7 +480,7 @@ export default function LiveSearch() {
                               gap: 1,
                             }}
                           >
-                            <CircularProgress size={16} />
+                            <CircularProgress size={16} color={clientTheme.colors.primaryContrastText}/>
                             Déposer
                           </Box>
                         ) : (
