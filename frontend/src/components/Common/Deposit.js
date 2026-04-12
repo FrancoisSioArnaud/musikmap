a97af7616bf22109a7a61443b6134d68c99ba03d
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-04-12 18:03:11 +0200

    Update Deposit.js

--- frontend/src/components/Common/Deposit.js
+++ frontend/src/components/Common/Deposit.js
@@ -470,7 +470,7 @@ export default function Deposit({
       }
 
       const linearProgress = Math.min((now - revealHoldStartRef.current) / HOLD_TO_REVEAL_MS, 1);
-      const progress = evaluateCubicBezier(linearProgress, 0,1.5,1,.2);
+      const progress = evaluateCubicBezier(linearProgress, 0,.94,1,.49);
       setHoldProgress(progress);
 
       if (linearProgress >= 1) {
