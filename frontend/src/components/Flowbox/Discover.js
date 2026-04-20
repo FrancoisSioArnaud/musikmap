22e66c401a7ae25c56002212fe87321d23dbbd2e
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-04-20 16:15:35 +0200

    Update Discover.js

--- frontend/src/components/Flowbox/Discover.js
+++ frontend/src/components/Flowbox/Discover.js
@@ -162,7 +162,7 @@ export default function Discover() {
 
         {mySong ? (
           <Box
-            className={`my_deposit deposit_card deposit_song${
+            className={`my_deposit deposit deposit_song${
               myDepositAccentColor ? " has_accent_color" : ""
             }`}
             style={
