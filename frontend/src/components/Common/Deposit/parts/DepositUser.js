58fff88ec969dec32e4183f752ff5ea859980ddb
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-04-30 12:05:19 +0200

    Update DepositUser.js

--- frontend/src/components/Common/Deposit/parts/DepositUser.js
+++ frontend/src/components/Common/Deposit/parts/DepositUser.js
@@ -4,11 +4,13 @@ import UserInline from "../../UserInline";
 
 export default function DepositUser({ user, onNavigateProfile }) {
   return (
-    <Typography variant="body1" component="span" sx={{ flex: "0 0 auto", whiteSpace: "nowrap" }}>Partagée par</Typography>
-    <UserInline
-      user={user}
-      className="deposit_user"
-      avatarSize={32}
-    />
+    <>
+      <Typography variant="body1" component="span" sx={{ flex: "0 0 auto", whiteSpace: "nowrap" }}>Partagée par</Typography>
+      <UserInline
+        user={user}
+        className="deposit_user"
+        avatarSize={32}
+      />
+    </>
   );
 }
