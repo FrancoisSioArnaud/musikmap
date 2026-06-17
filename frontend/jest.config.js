4328a36eb3cd638c3d7d6635e05ee3015b11c03f
FrancoisSioArnaud <127108555+FrancoisSioArnaud@users.noreply.github.com>
2026-06-17 20:11:19 +0200

    Delete frontend directory

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'json'],
-};
--- frontend/src/components/Common/Deposit/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Deposit } from "./Deposit";
-export { default as DepositSong } from "./parts/DepositSong";
